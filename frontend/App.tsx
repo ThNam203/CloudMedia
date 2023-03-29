75fefc7f1f5f49baa07880ec4bfff48f7ef124a0
Khooiiiii <21521010@gm.uit.edu.vn>
2023-03-29 22:22:20 +0700

    hello

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   let screen = <FirstTimeUseScreen />;
   return <View style={styles.container}>{screen}</View>;
 };
-
+hello;
 export default App;
 const styles = StyleSheet.create({
   container: {
