e829caf6a19e3068366095c78d7fc248dfd4bbb1
ThNam203 <hthnam203@gmail.com>
2023-04-01 18:49:43 +0700

    add login route

--- backend/index.js
+++ backend/index.js
@@ -3,28 +3,18 @@ require('./models/Mongoose')
 
 const express = require('express')
 
-const AppError = require('./utils/AppError')
-const authRouter = require('./routes/authRoutes')
+const errorHandlers = require('./controllers/errorController')
+const authRouter = require('./routes/recruiterAuthRoutes')
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.use('', authRouter)
+app.use('/recruiter', authRouter)
 
-app.use('*', (req, res, next) => {
-    next(new AppError('Invalid request url', 404))
-})
-
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Internal server error'
-    res.status(statusCode).json({
-        status: 'error',
-        message,
-    })
-})
+app.use('*', errorHandlers.invalidUrlHandler)
+app.use(errorHandlers.globalErrorHandler)
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server running on port ${process.env.SERVER_PORT}`)
