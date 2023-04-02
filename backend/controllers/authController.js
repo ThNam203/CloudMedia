bd0ebb4e8008f1281bc529d4b713b1a5cc80d525
ThNam203 <hthnam203@gmail.com>
2023-04-02 21:01:59 +0700

    refactor error handlers and add logout route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,45 +1,80 @@
 const jwt = require('jsonwebtoken')
-const RecruiterModel = require('../models/Recruiter')
+
+const User = require('../models/User')
+const JWTBlacklist = require('../models/JWTBlacklist')
 const AppError = require('../utils/AppError')
 const asyncCatch = require('../utils/asyncCatch')
-const Recruiter = require('../models/Recruiter')
 
-const getJWTToken = function (userId) {
-    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+const getJWTToken = function (userEmail) {
+    return jwt.sign({ email: userEmail }, process.env.JWT_SECRET, {
         expiresIn: '7 days',
     })
 }
 
-exports.signUpAsRecruiter = asyncCatch(async (req, res, next) => {
-    const newRecruiter = await RecruiterModel.create({
+exports.signUp = asyncCatch(async (req, res, next) => {
+    const newUser = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
+        isRecruiter: req.body.isRecruiter,
+        phoneNumber: req.body.phoneNumber,
+        createdDate: req.body.createdDate,
     })
 
-    res.status(404).json({
+    res.status(200).json({
         status: 'success',
-        data: `created a new user with id ${newRecruiter._id}`,
+        data: `created a new user with email ${newUser.email}`,
     })
 })
 
-exports.logInAsRecruiter = asyncCatch(async (req, res, next) => {
+exports.logIn = asyncCatch(async (req, res, next) => {
     const { email, password } = req.body
 
     if (!email || !password)
-        throw new AppError('Missing email or password', 400)
+        next(new AppError('Missing email or password', 400))
 
-    const freshUser = await Recruiter.findOne({ email }).select('+password')
-    if (!freshUser) throw new AppError('Email not found', 400)
+    const freshUser = await User.findOne({ email }).select('+password')
+    if (!freshUser) next(new AppError('Email not found', 400))
 
     if (!(await freshUser.checkPassword(password, freshUser.password)))
-        throw new AppError('Wrong password', 400)
+        next(new AppError('Wrong password', 400))
 
-    const jwtToken = getJWTToken(freshUser.id)
+    const jwtToken = getJWTToken(freshUser.email)
     res.status(200).json({
         status: 'success',
         data: {
             jwtToken: jwtToken,
         },
     })
+})
+
+exports.logOut = asyncCatch(async (req, res, next) => {
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+    ) {
+        const token = req.headers.authorization.split(' ')[1]
+
+        const freshToken = await JWTBlacklist.findOne({ jwtData: token })
+
+        if (freshToken)
+            return next(
+                new AppError(
+                    'Invalid request! Already logged out the user with this token',
+                    400
+                )
+            )
+
+        await JWTBlacklist.create({ jwtData: token })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                message: `Logout successfully`,
+            },
+        })
+    } else
+        return next(
+            new AppError('Missing or invalid authorization header', 400)
+        )
 })
