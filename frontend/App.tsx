8097684472d6aacb2227d04d08c01e6ad4dae48f
Khooiiiii <21521010@gm.uit.edu.vn>
2023-04-14 10:42:15 +0700

    Add StilHiringScreen and add Icon Libraries

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import FirstTimeUseScreen from './Screens/FirstTimeUseScreen';
 import ProfileScreen from './Screens/ProfileScreen';
+import StillHiringScreen from './Screens/StillHiringScreen';
 
 const App = () => {
-  let screen = <ProfileScreen />;
+  let screen = <StillHiringScreen />;
   return <View style={styles.container}>{screen}</View>;
 };
 export default App;
