9cad98d8d05c8e15dec9f35aa3d43fcc60559993
Son <sontruong02003@gmail.com>
2023-05-18 11:16:03 +0700

    fix bug met vcl

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -3,14 +3,17 @@ import {Provider} from 'react-redux';
 import {Store} from './reducers/Store';
 import {NavigationContainer} from '@react-navigation/native';
 import Routers from './Navigations/Routers';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const App = () => {
   return (
-    <Provider store={Store}>
-      <NavigationContainer>
-        <Routers />
-      </NavigationContainer>
-    </Provider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <Provider store={Store}>
+        <NavigationContainer>
+          <Routers />
+        </NavigationContainer>
+      </Provider>
+    </GestureHandlerRootView>
   );
 };
 
