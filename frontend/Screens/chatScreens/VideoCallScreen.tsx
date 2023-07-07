977ff8595c82089385354eb2ad40bb889edb240d
sen1or <21521010@gm.uit.edu.vn>
2023-07-07 10:23:44 +0700

    add stun servers to peer constraints

--- frontend/Screens/chatScreens/VideoCallScreen.tsx
+++ frontend/Screens/chatScreens/VideoCallScreen.tsx
@@ -33,15 +33,15 @@ export default function VideoCallScreen() {
 
   let peerConstraints = {
     iceServers: [
-      // {
-      //   urls: 'stun:stun.l.google.com:19302',
-      // },
-      // {
-      //   urls: 'stun:stun1.l.google.com:19302',
-      // },
-      // {
-      //   urls: 'stun:stun2.l.google.com:19302',
-      // },
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun1.l.google.com:19302',
+      },
+      {
+        urls: 'stun:stun2.l.google.com:19302',
+      },
     ],
   };
 
